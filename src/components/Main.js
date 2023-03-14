e0b34de9b001a6ea7ce32c70002f2c14342cd126
Vishal <74290663+dr-cyberx@users.noreply.github.com>
2023-03-14 10:46:35 +0530

    done with learning saga

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,25 +1,25 @@
-import { addToCart, emptyCart, removeToCart } from '../redux/action';
-import { useDispatch } from 'react-redux'
-import { productList } from '../redux/productAction';
-import {useSelector} from 'react-redux'
-import { useEffect } from 'react';
+import { addToCart, emptyCart, removeToCart } from "../redux/action";
+import { useDispatch } from "react-redux";
+import { productList } from "../redux/productAction";
+import { useSelector } from "react-redux";
+import { useEffect } from "react";
 
 function Main() {
   const dispatch = useDispatch();
-  let data = useSelector((state)=>state.productData);
+  let data = useSelector((state) => state.productData);
   console.warn("data in main component", data);
-  
-  useEffect(()=>{
-    dispatch(productList())
-  },[])
+
+  useEffect(() => {
+    dispatch(productList());
+  }, []);
   return (
     <div>
       <div>
-      <button onClick={() => dispatch(emptyCart())}>Empty Cart</button>
+        <button onClick={() => dispatch(emptyCart())}>Empty Cart</button>
       </div>
-      <div className='product-container'>
-        {
-          data.map((item)=><div className='product-item'>
+      <div className="product-container">
+        {data.map((item) => (
+          <div className="product-item">
             <img src={item.photo} alt="" />
             <div>Name : {item.name} </div>
             <div>Color : {item.color} </div>
@@ -27,12 +27,15 @@ function Main() {
             <div>Category : {item.category} </div>
             <div>Brand : {item.brand} </div>
             <div>
-              <button onClick={() => dispatch(addToCart(item))} >Add to Cart</button>
-              <button onClick={() => dispatch(removeToCart(item.id))}>Remove to Cart</button>
-
-              </div>
-          </div>)
-        }
+              <button onClick={() => dispatch(addToCart(item))}>
+                Add to Cart
+              </button>
+              <button onClick={() => dispatch(removeToCart(item.id))}>
+                Remove to Cart
+              </button>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,14 +1,14 @@
 // import {createStore} from 'redux'
-import {configureStore} from '@reduxjs/toolkit'
-import rootReducer from './rootReducer';
-import productSaga from './productSaga'
-import createSagaMiddleware from 'redux-saga';
+import { configureStore } from "@reduxjs/toolkit";
+import rootReducer from "./rootReducer";
+import productSaga from "./productSaga";
+import createSagaMiddleware from "redux-saga";
 
 // const store = createStore(rootReducer);
 const sagaMiddleware = createSagaMiddleware();
-const store  = configureStore({
-    reducer:rootReducer,
-    middleware:()=>[sagaMiddleware]
+const store = configureStore({
+  reducer: rootReducer,
+  middleware: () => [sagaMiddleware],
 });
 
 sagaMiddleware.run(productSaga);
